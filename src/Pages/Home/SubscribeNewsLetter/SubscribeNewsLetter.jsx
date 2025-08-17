627844d6313dc235f365469f821567bc1f485538
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-08-17 12:42:00 +0600

    sub now section responsive added

--- src/Pages/Home/SubscribeNewsLetter/SubscribeNewsLetter.jsx
+++ src/Pages/Home/SubscribeNewsLetter/SubscribeNewsLetter.jsx
@@ -3,36 +3,37 @@ import bgImg from "../../../assets/background/subscribe-section-bg.webp";
 import { IoMailOutline } from "react-icons/io5";
 const SubscribeNewsLetter = () => {
   return (
-    <div className="py-12 md:py-24 container mx-auto ">
-      <div className="flex flex-col md:flex-row  justify-center gap-8 border-2 border-light-text/50">
+    <div className="py-12 md:py-24 container mx-auto px-3 xl:px-0">
+      <div className="flex flex-col lg:flex-row items-center justify-center border-2 border-light-text/50 w-full">
         {/* Left side: BG image with icon */}
         <div
-          className="flex-1 h-96 md:h-[300px] bg-cover bg-center relative"
+          className="w-full xl:flex-1 lg:w-1/3 h-72 md:h-[200px] bg-cover bg-center relative"
           style={{ backgroundImage: `url(${bgImg})` }}
         >
           {/* Icon centered */}
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-6xl text-light-accent border-4 border-light-accent rounded-lg p-9">
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl md:text-6xl text-light-accent border-4 border-light-accent rounded-lg p-6 md:p-6 lg:p-9">
             <IoMailOutline />
           </div>
         </div>
 
         {/* Right side: Text */}
-        <div className="flex-2 bg-light-secondary pl-12 pt-12 h-full">
-          <h2 className="text-center md:text-left text-4xl xl:text-6xl text-light-text font-bold pb-6 font-secondary">
+        <div className="w-full xl:flex-2 lg:w-2/3 bg-light-secondary px-6 md:px-12 py-10 md:py-0 flex flex-col justify-center md:pb-6 lg:pb-0">
+          <h2 className="text-center lg:text-left text-3xl md:text-4xl xl:text-6xl text-light-text font-bold pb-4 md:pt-6 font-secondary">
             Subscribe Now
           </h2>
-          <p className="text-center md:text-left lg:text-xl pb-6 text-light-text">
+          <p className="text-center lg:text-left text-base md:text-lg lg:text-xl pb-3 lg:pb-6 text-light-text/60 font-secondary">
             Get the latest updates and news directly to your inbox.
           </p>
-          <form className=" w-9/12 flex">
+
+          <form className=" md:w-11/12 lg:w-9/12 flex mx-auto md:mx-0 pt-4 lg:pt-6">
             <input
               type="email"
               placeholder="Enter your email"
-              className="flex-1 px-4 py-3 rounded-l-md border border-light-text/60 focus:outline-none focus:ring-2 focus:ring-light-accent"
+              className="flex-1 p-2  md:px-4 md:py-3 rounded-l-md border border-light-text/60 focus:outline-none focus:ring-2 focus:ring-light-accent text-sm md:text-base"
             />
             <button
               type="submit"
-              className="px-6 py-3 bg-light-text text-light-primary font-bold rounded-r-md hover:bg-light-accent/90 transition-colors"
+              className="px-2 font-secondary md:px-6 py-3 bg-light-text text-light-primary md:font-bold rounded-r-md hover:bg-light-accent/90 transition-colors text-sm md:text-base"
             >
               Subscribe
             </button>
